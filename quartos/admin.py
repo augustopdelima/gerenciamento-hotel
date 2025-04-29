4d6fae1c932658d3a07a9c32c3fba872a0bee8d3
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2025-04-29 14:10:32 -0300

    feat(admin/quarto): Adiciona painel admin para gerenciar quartos

--- quartos/admin.py
+++ quartos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from quartos.models import Quarto
+
+
+@admin.register(Quarto)
+class QuartoAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'capacidade', 'status', 'tipo']
+    list_filter = ['tipo', 'capacidade', 'status']
+    search_fields = ['tipo__nome', 'status__tag']
