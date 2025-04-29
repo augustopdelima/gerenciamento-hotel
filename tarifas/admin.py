ac8b8f6b57160d1e3b509668ab13c7fbd1d6a594
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2025-04-29 14:03:52 -0300

    feat(admin-tarifas): Adiciona painel admin para gerenciar tarifas

--- tarifas/admin.py
+++ tarifas/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from tarifas.models import Tarifa
+
+
+@admin.register(Tarifa)
+class TarifaAdmin(admin.ModelAdmin):
+    list_display = [
+        'valor',
+        'tipo_quarto',
+        'ativa',
+        'data_inicio',
+        'data_fim',
+        'temporada'
+    ]
+
+    search_fields = [
+        'tipo_quarto__nome',
+        'data_inicio__nome',
+    ]
+
+    list_filter = [
+        'valor',
+        'tipo_quarto',
+        'ativa',
+        'temporada',
+        'data_inicio',
+        'data_fim'
+    ]
+
+    ordering = ['-data_inicio', 'tipo_quarto']
