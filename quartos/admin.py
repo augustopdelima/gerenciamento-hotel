de11cb21a785839de1ab54883fd8bc148b2d0b1c
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2025-04-29 14:14:23 -0300

    feat(admin/quarto): Adiciona ordem por número e capacidade no painel de admin dos quartos

--- quartos/admin.py
+++ quartos/admin.py
@@ -8,6 +8,7 @@ class QuartoAdmin(admin.ModelAdmin):
     list_display = ['numero', 'capacidade', 'status', 'tipo']
     list_filter = ['tipo', 'capacidade', 'status']
     search_fields = ['tipo__nome', 'status__tag']
+    ordering = ['numero', 'capacidade']
 
 
 @admin.register(StatusQuarto)
