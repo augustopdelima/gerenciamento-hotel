5a053046f627c6e5c30bc7083f8a6c3dd5de9127
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2025-04-29 14:12:45 -0300

    feat(admin/quarto/status): Adiciona painel admin para gerenciar status dos quartos

--- quartos/admin.py
+++ quartos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from quartos.models import Quarto
+from quartos.models import Quarto, StatusQuarto
 
 
 @admin.register(Quarto)
@@ -8,3 +8,10 @@ class QuartoAdmin(admin.ModelAdmin):
     list_display = ['numero', 'capacidade', 'status', 'tipo']
     list_filter = ['tipo', 'capacidade', 'status']
     search_fields = ['tipo__nome', 'status__tag']
+
+
+@admin.register(StatusQuarto)
+class StatusQuartoAdmin(admin.ModelAdmin):
+    list_display = ['tag']
+    list_filter = ['tag']
+    search_fields = ['tag']
