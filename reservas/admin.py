660068f802cbdf4fd4320c12f8bd08089ce8ef66
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2025-04-29 15:29:04 -0300

    feat(admin/reserva): Adiciona painel admin para gerenciar reservas

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from reservas.models import Reserva
+
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = [
+        'data_entrada',
+        'data_saida',
+        'cliente',
+        'funcionario',
+        'status',
+        'quarto',
+        'criada_em'
+    ]
+
+    list_filter = [
+        'funcionario',
+        'data_entrada',
+        'data_saida',
+        'status',
+        'cliente',
+        'quarto',
+        'criada_em'
+    ]
+
+    search_fields = [
+        'quarto__numero',
+        'status__tag',
+        'data_entrada',
+        'data_saida',
+        'funcionario__nome',
+        'cliente__nome'
+    ]
