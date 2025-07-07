35c9cac77368b62c81cd6c0b2de8aa6ab47f845d
Augusto Pasquali <augusto.pasqualidelima@gmail.com>
2025-07-07 18:43:44 -0300

    Add: detalhes reserva

--- ocorrencias/forms.py
+++ ocorrencias/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Ocorrencia
+from quartos.models import Quarto
 
 
 class OcorrenciaForm(forms.ModelForm):
@@ -16,3 +17,50 @@ class OcorrenciaForm(forms.ModelForm):
         widgets = {
             'descricao': forms.Textarea(attrs={'rows': 4}),
         }
+
+
+class RelatorioOcorrencias(forms.Form):
+    data_inicio = forms.DateTimeField(
+        required=False,
+        label="Data de Início",
+        widget=forms.DateTimeInput(
+            attrs={'type': 'datetime-local', 'class': 'form-control'}
+        ),
+        input_formats=['%Y-%m-%dT%H:%M'],
+    )
+
+    data_fim = forms.DateTimeField(
+        required=False,
+        label="Data de Fim",
+        widget=forms.DateTimeInput(
+            attrs={'type': 'datetime-local', 'class': 'form-control'}
+        ),
+        input_formats=['%Y-%m-%dT%H:%M'],
+    )
+
+    quarto = forms.ModelChoiceField(
+        required=False,
+        label="Quarto",
+        queryset=Quarto.objects.order_by('numero'),
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+    resolvido = forms.ChoiceField(
+        required=False,
+        label="Resolvido",
+        choices=[
+            ('', 'Todos'),
+            ('sim', 'Resolvidos'),
+            ('nao', 'Pendentes'),
+        ],
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+    criado_por = forms.CharField(
+        required=False,
+        label="Criado por (usuário)",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'username'
+        })
+    )
